66eda013318f840411570d9cf510801dfc2ce575
PraderioM <PraderioM@Gmail.com>
2017-10-14 23:46:17 +0200

    added function.

--- read_data_from_database.py
+++ read_data_from_database.py
@@ -15,8 +15,22 @@ def get_sensor_information(sensor):
     out[1] = bool(out[1])
     out[0][0] = float(out[0][0])
     out[0][1] = float(out[0][1])
+    time = data['observations'][0]["timestamp"]
+    time = time.split("T")
+    time[1] = time[1].split(":")
+    time[0] = time[0].split("/")
+    date = [int(time[0][2]), int(time[0][1]), int(time[0][0]), int(time[1][0]), int(time[1][1]), int(time[1][2])]
+    out.append(date)
     return out
 
 
-data = get_sensor_information("luminosity1")
+def put_sensor_information(sensor, value):
+    headers = {'IDENTITY_KEY': 'e3ba38839c9f601471b8f3f662a38ae23473bebf63cec53a183edec2d5c84e75'}
+    resp = requests.put("http://api.thingtia.cloud/data/provedor1/"+sensor+"/"+str(value), headers=headers)
+    if not resp.ok:
+        resp.raise_for_status()
+
+
+
+data = get_sensor_information("Luminosity1")
 print(data)
